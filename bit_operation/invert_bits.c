fa2c8f2ea1309fbbd56da1f6c8fe1d3ad4f45c79
elangoelinuxer <ela.ece76@gmail.com>
2020-03-07 14:27:28 +0530

    inverti bits

---
+++ bit_operation/invert_bits.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void print_binary(int *rec_val)
+{
+	for(int i=31; i>=0;--i)
+	{
+		if(*rec_val & 1 << i)
+			printf("1 ");
+		else
+			printf("0 ");
+	}
+	printf("\n");
+}
+
+void invert_bits(int *rec_val)
+{
+
+*rec_val= ~(*rec_val);
+
+}
+
+int main()
+{
+	int *input_val;
+	input_val = malloc(sizeof(int));
+	printf("Enter input: ");
+	scanf("%d",input_val);
+	printf("Input Entered:  %d\n",*input_val);
+	print_binary(input_val);
+	
+	invert_bits(input_val);
+	
+	print_binary(input_val);
+}
+
+
+
